2ea6c9cf81fd5945358895be9223118d0788e83a
JHay0112 <jordanhay0112@gmail.com>
2019-11-26 12:13:02 +1300

    Metadata update

--- res/head.php
+++ res/head.php
@@ -15,9 +15,16 @@
     <head>
         <!-- Meta Data -->
         <meta charset="UTF-8" />
-        <title><?php print($title) ?> - Trails</title>
+        <meta name="description" content="Trails and Tracks in Timaru." />
+        <meta name="keywords" content="Timaru, Trails, New Zealand, Walking, Timaru Trails, Hiking, Strolling, South Canterbury, Canterbury" />
+        <meta name="author" content="Jordan Hay" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <!-- Stylesheets -->
-        <link rel="stylesheet" type="text/css" href="css/styles.css">
+        <link rel="stylesheet" type="text/css" href="css/styles.css" />
+
+        <!-- Tab Info -->
+        <link rel="icon" href="img/favicon.ico" />
+        <title><?php print($title) ?> - Timaru Trails</title>
     </head>
     <body>
