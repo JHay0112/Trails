29de4ce612b1c14f02ee13697490597e0cddd49a
JHay0112 <jordanhay0112@gmail.com>
2019-11-27 11:41:50 +1300

    Google Analytics

--- res/foot.php
+++ res/foot.php
@@ -1,5 +1,16 @@
         <footer id="page-footer">
             <small><?php print($title); ?> - Timaru Trails | &copy; <a href="https://jordanhay.tk/" target="_blank">Jordan Hay</a></small>
         </footer>
+
+        <!-- Scripts -->
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109227996-3"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-109227996-3');
+        </script>
     </body>
 </html>
