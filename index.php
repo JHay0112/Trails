de0920e79a1601b690586096f5ec109f95b2d922
JHay0112 <jordanhay0112@gmail.com>
2019-11-26 21:18:22 +1300

    index.php style update

--- index.php
+++ index.php
@@ -10,15 +10,15 @@
 ?>
 
 <main id="index-body">
-    <div class="col-2 desktop-only"></div>
-    <section id="content" class="col-8">
+    <div class="col-3 desktop-only"></div>
+    <section id="content" class="col-6">
         <h1>Timaru Trails</h1>
         <hr />
         <?php require("res/nav.php"); ?>
         <hr />
         <p>Looking for walking tracks or hikes in Timaru and South Canterbury? Timaru Trails has a vast collection of trails for you to try. Just click on "Search Trails" to get started.</p>
     </section>
-    <div class="col-2 desktop-only"></div>
+    <div class="col-3 desktop-only"></div>
 </main>
 
 <?php require("res/foot.php"); ?>
