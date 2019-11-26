a554bddcff9940cc344e6cb7e20566c26bd30215
JHay0112 <jordanhay0112@gmail.com>
2019-11-26 19:20:46 +1300

    index.php setup

--- index.php
+++ index.php
@@ -9,6 +9,20 @@
     require("res/head.php"); // Require template for top of page
 ?>
 
-<h1>Trails</h1>
+<main id="index-body">
+    <div class="col-2 desktop-only"></div>
+    <section id="content" class="col-8">
+        <h1>Timaru Trails</h1>
+        <hr />
+        <nav>
+            <a href="/" class="active">Home</a>
+            <a href="search.php">Search Trails</a>
+            <a href="about.php">About</a>
+        </nav>
+        <hr />
+        <p>Looking for walking tracks or hikes in Timaru and South Canterbury? Timaru Trails has a vast collection of trails for you to try. Just click on "Search Trails" to get started.</p>
+    </section>
+    <div class="col-2 desktop-only"></div>
+</main>
 
-<?php require("res/foot.php"); // Require template for bottom of page ?>
+<?php require("res/foot.php"); ?>
