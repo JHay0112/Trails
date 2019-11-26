c199d592e6d1f48b8849c7ccfff7427c54e7333a
JHay0112 <jordanhay0112@gmail.com>
2019-11-26 22:31:41 +1300

    Basic Generic Page Header

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+
+    /*
+        header.php
+        Author: Jordan Hay
+    */
+
+?>
+
+<header>
+    <div id="banner"></div>
+
+    <h1><?php print($title); ?> - Timaru Trails</h1>
+
+    <?php require("res/nav.php"); ?>
+</header>
