13f8a3e3079368ae43d3c2ed486e563c167a663e
JHay0112 <jordanhay0112@gmail.com>
2019-11-26 13:37:36 +1300

    Footer Setup

--- res/foot.php
+++ res/foot.php
@@ -1,5 +1,5 @@
-        <footer>
-            <p><?php print($title); ?> - Trails | &copy; Jordan Hay</p>
+        <footer id="page-footer">
+            <small><?php print($title); ?> - Timaru Trails | &copy; <a href="https://jordanhay.tk/" target="_blank">Jordan Hay</a></small>
         </footer>
     </body>
 </html>
