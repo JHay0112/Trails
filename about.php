de335619824aa7b9340a614e87f5d1cb06e0b452
JHay0112 <jordanhay0112@gmail.com>
2019-11-27 11:14:58 +1300

    Header site wide, nav update

--- about.php
+++ about.php
@@ -7,6 +7,7 @@
     $title = "About"; // Set title for head.php
 
     require("res/head.php"); // Require template for top of page
+    require("res/header.php");  // Header template
 ?>
 
 <?php require("res/foot.php"); ?>
--- search.php
+++ search.php
@@ -6,7 +6,8 @@
 
     $title = "Search Trails"; // Set title for head.php
 
-    require("res/head.php"); // Require template for top of page
+    require("res/head.php"); // Require template for top of page/metadata
+    require("res/header.php");  // Header template
 ?>
 
 <?php require("res/foot.php"); ?>
