0f08e32251e903f21850c336fd81e6be24acd625
JHay0112 <jordanhay0112@gmail.com>
2019-11-26 19:23:43 +1300

    Very basic page setup

---
+++ about.php
@@ -0,0 +1,12 @@
+<?php
+    /*
+        about.php
+        Author(s): Jordan Hay
+    */
+
+    $title = "About"; // Set title for head.php
+
+    require("res/head.php"); // Require template for top of page
+?>
+
+<?php require("res/foot.php"); ?>
---
+++ search.php
@@ -0,0 +1,12 @@
+<?php
+    /*
+        search.php
+        Author(s): Jordan Hay
+    */
+
+    $title = "Search Trails"; // Set title for head.php
+
+    require("res/head.php"); // Require template for top of page
+?>
+
+<?php require("res/foot.php"); ?>
