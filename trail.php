19da124086aa93043c45807b13aea67232093eb4
JHay0112 <jordanhay0112@gmail.com>
2019-11-27 13:49:25 +1300

    Basic Trail Page

---
+++ trail.php
@@ -0,0 +1,15 @@
+<?php
+    /*
+        trail.php
+        Author(s): Jordan Hay
+    */
+
+    require("res/connect.php"); // Connect to database
+
+    $title = ""; // Set title for head.php
+
+    require("res/head.php"); // Require template for top of page/metadata
+    require("res/header.php");  // Header template
+?>
+
+<?php require("res/foot.php"); ?>
