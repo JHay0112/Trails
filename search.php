1ba4b38f7efd9084d63af29e9d1a34e6af7b68a3
JHay0112 <jordanhay0112@gmail.com>
2019-11-27 22:31:48 +1300

    Minor Updates

--- search.php
+++ search.php
@@ -4,12 +4,11 @@
         Author(s): Jordan Hay
     */
 
-    require("res/connect.php"); // Connect to database
-
     $title = "Search Trails"; // Set title for head.php
 
     require("res/head.php"); // Require template for top of page/metadata
     require("res/header.php");  // Header template
+    require("res/connect.php"); // Connect to database
 ?>
 
 <?php require("res/foot.php"); ?>
