42a1bdb556c19cc837eeccbd1387a6c8c5da938a
JHay0112 <jordanhay0112@gmail.com>
2019-11-29 15:51:45 +1300

    Very basic page layout

--- search.php
+++ search.php
@@ -11,4 +11,13 @@
     require("res/connect.php"); // Connect to database
 ?>
 
+<main class="col-12">
+    <div class="col-2"></div>
+    <section class="col-8">
+        <h2>Search</h2>
+        
+    </section>
+    <div class="col-2"></div>
+</main>
+
 <?php require("res/foot.php"); ?>
