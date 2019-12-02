e2211ea2f6cb865c89beff674244a8459c57dbd6
JHay0112 <jordanhay0112@gmail.com>
2019-12-03 11:28:38 +1300

    Credit where credit's due

--- about.php
+++ about.php
@@ -16,11 +16,12 @@
         <p>Timaru Trails was thought up by <a href="https://jordanhay.tk/" target="_blank">Jordan Hay</a> in early 2018. In late 2019 he actually bothered to make it.</p>
         <p>Timaru Trails is designed to document all the trails in Timaru and eventually South Canterbury to allow people who live in or are visting South Canterbury to easily find a wide selection of trails to enjoy.</p>
         <p>All trails have been documented, photographed, and mapped by <a href="https://jordanhay.tk/" target="_blank">Jordan Hay</a>.</p>
+        <p>Trail descriptions were written by Tanya Smith.</p>
         <p>You can scrutinise the code that makes this website run <a href="https://github.com/JHay0112/Trails" target="_blank">here</a>.</p>
     </section>
     <section class="col-8">
         <h2>Jordan Hay</h2>
-        <p>Jordan Hay is a student with interests in robotics, programming, web development, and stereotypically enough, maths.</p>
+        <p>Jordan Hay is a student with interests in robotics, programming, web development, and stereotypically enough, maths. Jordan Hay built Timaru Trails in his spare time on a budget of 10 cents he found in the pocket of his jeans.</p>
         <p>You can find his website <a href="https://jordanhay.tk/" target="_blank">here</a>.</p>
     </section>
 </main>
