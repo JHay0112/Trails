d4f568e9141cae1d2fe0a7d16d853e36139faff6
JHay0112 <jordanhay0112@gmail.com>
2019-12-02 19:26:24 +1300

    Mistakes were made, reverted nav a order

--- res/nav.php
+++ res/nav.php
@@ -18,6 +18,6 @@
     <a href="javascript:void(0)" id="nav-toggle" onclick="toggleNav()"><span class="fa fa-bars"></span></a>
     <a href="index.php" class="<?php checkIfActive("Home"); ?>">Home</a>
     <a href="search.php" class="<?php checkIfActive("Search Trails"); ?>" >Search Trails</a>
-    <?php if($found_active_page == false){print("<a href=\".$title.\" class=\"active\">".$title."</a>");}  // Check if expected page is found, if not add to nav ?>
     <a href="about.php" class="<?php checkIfActive("About"); ?>" >About</a>
+    <?php if($found_active_page == false){print("<a href=\".$title.\" class=\"active\">".$title."</a>");}  // Check if expected page is found, if not add to nav ?>
 </nav>
