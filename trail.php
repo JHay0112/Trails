dd3364c1029bf3a94888c7df173769c89abd9888
JHay0112 <jordanhay0112@gmail.com>
2019-12-04 13:36:14 +1300

    Attribute icons

--- trail.php
+++ trail.php
@@ -8,24 +8,36 @@
 
     try {
         // Querying the database for selected trail
-        $query = "SELECT * FROM `trails` WHERE `trail_id` = ?;";
-        $stmt = $link->prepare($query);
+        $sql = "SELECT * FROM `trails` WHERE `trail_id` = ?;";
+        $stmt = $link->prepare($sql);
         $stmt->bind_param("s", $_GET["trail"]);
         $stmt->execute();
         $stmt->bind_result($trail_id, $trail_name, $trail_area, $trail_desc, $trail_map, $trail_dist, $trail_walk_time, $trail_elevation_change);
         $stmt->fetch();
+        $stmt->close();
     } catch(\Exception $e) {
-        print("Failed to connect to Database");
+        // Make sure to set trail_id to nothing so that the trail results get filled with trail not found values
+        $trail_id = "";
     }
 
+    // $trail_id is a safe value to put into the queries without preparing as it is a known and valid value
+
     // If trail does not exist fill with artificial values
-    if($trail_name == ""){
+    if($trail_id == ""){
         $trail_name = "Trail Not Found";
         $trail_area = "Nowhere?";
         $trail_desc = "Uh oh. This trail does not exist. We're sorry for any inconvenience. Click <a href=\"search.php\">here</a> to return to the the search page.";
         $trail_dist = 0;
         $trail_walk_time = 0;
         $trail_elevation_change = 0;
+    } else {
+        // We know that the trail we have is valid, now we need to load the attributes of the trail
+        try {
+            $sql = "SELECT * FROM `attributes` WHERE `attribute_id`IN (SELECT `attribute_id` FROM `trail_attributes` WHERE `trail_id` = ".$trail_id.");";
+            $attributes = $link->query($sql);
+        } catch(\Exception $e) {
+            // Do nothing
+        }
     }
 
     $title = $trail_name;
@@ -38,6 +50,16 @@
     <section class="col-8" id="trail-main">
         <article class="col-7" id="trail-info">
             <h4><i class="fa fa-map-marker"></i> <?php print($trail_area); ?></h4>
+            <section class="col-12" id="attribute-container">
+                <?php
+                    while($attribute = $attributes->fetch_assoc()) {
+                        print("<div class=\"attribute\">");
+                        print("<img class=\"attribute-image\" src=\"img/attr/".$attribute["attribute_id"].".png\" alt=\"".$attribute["attribute_name"]."\" />");
+                        print("<p class=\"attribute-text\">".$attribute["attribute_desc"]."</p>");
+                        print("</div>");
+                    }
+                ?>
+            </section>
             <h5>Distance: ~<?php 
                 // Round distances
                 if (strlen((string)$trail_dist) == 5) {
