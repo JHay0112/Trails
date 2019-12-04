4e9fe5a5992932c543a949527a30770a1f68e0f5
JHay0112 <jordanhay0112@gmail.com>
2019-12-04 20:10:18 +1300

    Social hover over update

--- res/foot.php
+++ res/foot.php
@@ -6,15 +6,15 @@
 
     // Used to print out the URL, allows this to not need changing if domain gets changed
     function getHost() {
-        print($_SERVER['HTTP_HOST']);
+        print($_SERVER["HTTP_HOST"]);
     }
 ?>
         <footer id="page-footer">
             <small>
                 <?php print($title); ?> - Timaru Trails <p class="desktop-only">|</p><br class="mobile-only" />
-                <a href="https://www.facebook.com/sharer/sharer.php?u=<?php getHost(); ?>" class="fa fa-facebook" target="_blank"></a>
-                <a href="https://twitter.com/intent/tweet?text=<?php getHost(); ?>" class="fa fa-twitter" target="_blank"></a>
-                <a href="https://www.reddit.com/submit?url=<?php getHost(); ?>" class="fa fa-reddit" target="_blank"></a>
+                <a href="https://www.facebook.com/sharer/sharer.php?u=<?php getHost(); ?>" class="fa fa-facebook social" target="_blank"></a>
+                <a href="https://twitter.com/intent/tweet?text=<?php getHost(); ?>" class="fa fa-twitter social" target="_blank"></a>
+                <a href="https://www.reddit.com/submit?url=<?php getHost(); ?>" class="fa fa-reddit social" target="_blank"></a>
                 | &copy; <a href="https://jordanhay.tk/" target="_blank">Jordan Hay</a> 2019
             </small>
         </footer>
