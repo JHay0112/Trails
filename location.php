16e455a7daa5d0bea9075914eab0b3d8dcb30104
JHay0112 <jordanhay0112@gmail.com>
2019-12-22 22:08:21 +1300

    Location Page

---
+++ location.php
@@ -0,0 +1,13 @@
+<?php  
+    /*
+        location.php
+        Author: Jordan Hay 
+    */
+
+    require("res/connect.php"); // Connect to database
+
+    require("res/head.php"); // Require template for top of page/metadata
+    require("res/header.php");  // Header template
+?>
+
+<?php require("res/foot.php"); ?>
